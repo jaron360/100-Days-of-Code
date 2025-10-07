0867c174a140a2a08a681dc6949e44af841b4978
Jaron <jdbedwards5@gmail.com>
2025-10-07 13:00:01 -0700

    Update CaesarCipher.py

--- Day 8 - Function Parameters and Caesar Cipher/CaesarCipher.py
+++ Day 8 - Function Parameters and Caesar Cipher/CaesarCipher.py
@@ -1,35 +1,43 @@
+import art
+print(art.logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+go_again = "yes"
+while go_again == "yes":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
-def encrypt(original_text, shift_amount):
-    cipher_text = ""
-    for letter in original_text:
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        cipher_text += alphabet[shifted_position]
+    def encrypt(original_text, shift_amount):
+        cipher_text = ""
+        for letter in original_text:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            cipher_text += alphabet[shifted_position]
 
-    print(f"Here is the encoded result: {cipher_text}")
-
-
-def decrypt(original_text, shift_amount):
-    decoded_result = ""
-    for letter in original_text:
-        shifted_position = alphabet.index(letter) - shift_amount
-        shifted_position %= len(alphabet)
-        decoded_result += alphabet[shifted_position]
-    print(f"Here is the Decoded result: {decoded_result}")
+        print(f"Here is the encoded result: {cipher_text}")
 
 
-if direction == "encode":
-    encrypt(text,shift)
-elif direction == "decode":
-    decrypt(text,shift)
-else:
-    print("Please enter a valid choice")
+    def decrypt(original_text, shift_amount):
+        decoded_result = ""
+        for letter in original_text:
+            shifted_position = alphabet.index(letter) - shift_amount
+            shifted_position %= len(alphabet)
+            decoded_result += alphabet[shifted_position]
+        print(f"Here is the Decoded result: {decoded_result}")
 
 
+    if direction == "encode":
+        encrypt(text,shift)
+    elif direction == "decode":
+        decrypt(text,shift)
+    else:
+        print("Please enter a valid choice")
 
-
+    go_again = input("Would you like to go again? ")
+    if go_again == "yes":
+        continue
+    else:
+        print("Goodbye")
+        break
